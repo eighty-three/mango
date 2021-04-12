c2f2e916f5e63273e3444561764b6999e35ccd8b
Eighty Three <contact@eighty-three.dev>
2021-04-12 15:25:56 +0800

    feat: add nprogress to pages

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import '@/styles/global.css';
+
+//NProgress
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import '@/styles/nprogress.scss';
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const propTypes = {
   Component: PropTypes.elementType,
