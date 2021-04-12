b72c20568291551d59288f1674e2babaf4c172ee
Eighty Three <contact@eighty-three.dev>
2021-04-12 13:39:19 +0800

    fix: add new columns, fix modified columns
    Lowercase strings to follow enum in database
    
    Remove empty string for TLanguage and TPublicationStatus
    
    'null' strings are for representing the null option when searching
    (i.e. for rows where its value is specifically null, instead of simply
    not selecting anything)

--- api/src/manga/schema.ts
+++ api/src/manga/schema.ts
@@ -1,7 +1,9 @@
 import Joi from '@hapi/joi';
 
-const languages = ['cn', 'jp', 'kr', 'gb']; // gb = en
-const pub_status = ['Ongoing', 'Complete', 'Hiatus', 'Cancelled']; // fix
+const languages = ['cn', 'jp', 'kr', 'gb', 'hk']; // gb = en
+const publication = ['ongoing', 'complete', 'hiatus', 'cancelled'];
+const reading = ['dropped', 'completed', 'reading', 'null'];
+const downloaded = ['complete', 'incomplete', 'none', 'null'];
 
 export const getSeries = Joi.object({
   id: Joi.number().required()
@@ -16,5 +18,8 @@ export const searchMetadata = Joi.object({
   author: Joi.string(),
   artist: Joi.string(),
   language: Joi.string().valid(...languages),
-  pub_status: Joi.string().valid(...pub_status)
+  publication: Joi.string().valid(...publication),
+  reading: Joi.string().valid(...reading),
+  downloaded: Joi.string().valid(...downloaded),
+  from_md: Joi.boolean(),
 });
