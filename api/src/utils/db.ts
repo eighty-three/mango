875e7ec1fd5d86d51c47729756c96b47fcc94d45
Eighty Three <contact@eighty-three.dev>
2021-04-12 13:30:08 +0800

    chore: fix schema and automate tables creation
    Fix 'tables' text to fit query format. Add 'if not exists' qualifier to
    all commands.
    
    Add new columns: 'reading', 'downloaded', 'from_md'
    Modify columns: 'language', 'pub_status' (to 'publication')
    Create enums for the mentioned columns.
    
    Create 'createTables' function to automate creation of new tables,
    copying the query text from the 'tables' file

--- api/src/utils/db.ts
+++ api/src/utils/db.ts
@@ -1,20 +1,40 @@
-import config from '../utils/config';
-import pgp from 'pg-promise';
+import config from '@utils/config';
 import logger from '@utils/logger';
+import pgp from 'pg-promise';
+import fs from 'fs';
 
 const pg = pgp();
 const cn = `postgresql://${config.DB_USERNAME}:${config.DB_PASSWORD}@${config.DB_HOSTNAME}:${config.DB_PORT}/${config.DB_PROJNAME}`;
 const db = pg(cn);
+
+const createTables = async (): Promise<void> => {
+  try {
+    const schema = fs.readFileSync('../tables', 'utf8');
+    await db.none(schema);
+
+    logger.info('    Create tables success');
+  } catch (err) {
+    if (err.message !== 'Invalid query format') {
+      logger.error('    CREATE TABLES FAILED: invalid query format for tables file');
+    } else {
+      logger.error(err);
+    }
+  }
+};
 
 // tests connection and prints out Postgres server version,
 // if successful; or else rejects with connection error:
 (async () => {
   let dbConnection;
   try {
+
     dbConnection = await db.connect();
     logger.info(`successfully connected to postgres database ${config.DB_PROJNAME}.
     server version: ${dbConnection.client.serverVersion}
     port: ${config.PORT} host: ${config.DB_HOSTNAME}.`);
+
+    logger.info('\n    Creating tables...');
+    await createTables();
   } catch (err) {
     logger.error(`error connecting to postgres database ${config.DB_PROJNAME} error: ${err}`)
   } finally {
